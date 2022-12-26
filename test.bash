285d15cb0b674167c8752cd4f00ae71415f9db75
root <root@DESKTOP-V0CH8E9.localdomain>
2022-12-26 20:05:19 +0900

    Add a command

---
+++ test.bash
@@ -0,0 +1,9 @@
+#!/bin/bash -xv 
+# SPDX-FileCopyrightText: 2022 Hayato Aizawa
+# SPDX-License-Identifier: BSD-3-Clause
+
+s = input()
+
+print( "変換後:" + s.swapcase())
+Num = len(s)
+print( "文字数:" + str(Num))
