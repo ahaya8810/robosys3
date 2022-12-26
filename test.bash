cbb1218a72845939babd55cda3c85e57f09d713a
root <root@DESKTOP-V0CH8E9.localdomain>
2022-12-26 21:31:02 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -2,8 +2,26 @@
 # SPDX-FileCopyrightText: 2022 Hayato Aizawa
 # SPDX-License-Identifier: BSD-3-Clause
 
-s = input()
+ng () {
+	echo NG at Line $1
+		res=1
+}
 
-print( "変換後:" + s.swapcase())
-Num = len(s)
-print( "文字数:" + str(Num))
+res=0
+
+###l/O TEST###
+out=$(./conversion<sentence)
+["${out}"="変換後:gOLD　rOGER, tHE "tHE KING OF THE PIRATES," HAD ACHIEVED IT ALL.
+文字数:63"]||ng $LINENO
+
+### STRANGE INPUT ###
+out=$(cat 1 |./conversion)
+[ "$?" = 1 ]      || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
+out=$(cat あ | ./conversion)
+[ "$?" = 1 ]      || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
+[ "$res" = 0 ] && echo OK
+exit $res
